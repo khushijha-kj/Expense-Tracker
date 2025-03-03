ac5c95fbe03d14bb7f20e4413d36055223479214
khushijha-kj <khushijhakj2311@gmail.com>
2025-03-03 20:01:21 +0000

    m

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import  cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js";
 
 
 dotenv.config({});
@@ -21,6 +22,9 @@ const corsOptions = {
   credentials:true
 }
 app.use(cors(corsOptions));
+
+//apis
+app.use("/api/v1/user", userRoute); 
 
 
 app.listen(PORT, () => {
