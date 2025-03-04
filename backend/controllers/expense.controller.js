22fafbad6a7a0760e38da4d4160584c408b6e582
khushijha-kj <khushijhakj2311@gmail.com>
2025-03-04 03:40:59 +0000

    Add controllers

---
+++ backend/controllers/expense.controller.js
@@ -0,0 +1,80 @@
+import { query } from 'express';
+import {Expense} from '../models/expense.model.js';
+export const addExpense = async (req,res) => {
+    try{
+        const {description, amount, category} = req.body;
+        const userId = req.id;
+        if(!description || !amount || !category) {
+            return res.status(400).json({message: "Please fill in all fields.",success: false})
+        };
+        const expense = await Expense.create({ 
+            description,
+            amount,
+            category,
+            userId
+        });
+        return res.status(201).json({message: "Expense added successfully",success: true,expense});
+
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+export const getAllExpenses = async (req,res) => {
+    try{
+        const userId = req.id;
+        let category = req.query.category || "";
+        const done = req.query.category || "";
+
+        const query = {
+            userId
+        }
+
+        if(category.toLowerCase() === "all"){
+            //no need to filter by category
+
+        }else{
+            query.category = {$regex:category,$options:'i'}
+        }
+        if(done.toLowerCase() === "done"){
+            query.done = true;
+
+        }
+        else if(done.toLowerCase() === "undone"){
+            query.done = false;
+            
+        };
+        const  expense = await Expense.find(query);
+        if(!expense || expense.length === 0){
+            return res.status(404).json({message:"No expense found",success:false})
+        };
+        return res.status(200).json({
+            message,
+            success:true,
+        })
+    }
+    catch(error){
+        console.log(error);
+
+    }
+}
+
+export const markAsDone = async(req,res) => {
+    try{
+        const expenseId = req.params.id;
+        const done = req.body;
+        const expense = await Expense.findByIdAndUpdate(expenseId,done,{new:true});
+        if(!expense){
+            return res.status(404).json({
+                message
+            })
+        }
+
+
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({ message: "Server error", success: false });
+    }
+}
